4ed8b2a735c31bb89264462183de30e393a1f4eb
Pat <pgulotta@gmail.com>
2019-11-18 13:53:28 -0600

    Added support for command line parms

--- src/lib/main.cpp
+++ src/lib/main.cpp
@@ -7,12 +7,38 @@
 #include <pistache/common.h>
 #include "requesthandler.hpp"
 #include "symart_service.hpp"
-#include <QImage>
-
+#include <QCoreApplication>
+#include <QCommandLineParser>
 
 using namespace Pistache;
 
-int main( int, char* [] )
+int main( int argc, char* argv[] )
 {
-  RequestHandler requestHandler{60564};
+  QCoreApplication app( argc, argv );
+  QCoreApplication::setApplicationName( "symart_server" );
+  QCoreApplication::setApplicationVersion( "1.0" );
+  QCommandLineParser parser;
+  parser.setApplicationDescription( "Service that generates symmetric images" );
+  parser.addHelpOption();
+  parser.addVersionOption();
+
+  QCommandLineOption definePortOption( QStringList() << "p" << "service-port",
+                                       QCoreApplication::translate( "main", "Port the service listens to for symart requests." ),
+                                       QCoreApplication::translate( "main", "port" ) );
+  parser.addOption( definePortOption );
+  const QCommandLineOption helpOption = parser.addHelpOption();
+  const QCommandLineOption versionOption = parser.addVersionOption();
+  parser.process( app );
+
+  int port{60564};
+
+  if ( parser.isSet( definePortOption ) ) {
+    auto definedPort =  parser.value( definePortOption ).toInt();
+
+    if ( definedPort > 0 ) {
+      port = definedPort;
+    }
+  }
+
+  RequestHandler requestHandler{port};
 }
