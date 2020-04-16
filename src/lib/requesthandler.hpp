aca4cda1f66cd84ba704a3b52c01c7064e850a44
pgulotta <pgulotta@gmail.com>
2020-04-15 19:04:42 -0500

    changing wallpaper size

--- src/lib/requesthandler.hpp
+++ src/lib/requesthandler.hpp
@@ -3,14 +3,14 @@
 
 /*  Examples
 
-http://localhost:60566/ready
-http://localhost:60566/test?trap
-http://loclahost:60566/test?walk
-http://localhost:60566/test?walk2
-http://localhost:60566/test?squiggles
+http://localhost:60564/ready
+http://localhost:60564/test?trap
+http://loclahost:60564/test?walk
+http://localhost:60564/test?walk2
+http://localhost:60564/test?squiggles
 http://localhost:60564/test?hyperbolicLines
 http://localhost:60564/test?quasiTrap
-http://localhost:60566/wallpaper
+http://localhost:60564/wallpaper
 
 */
 
